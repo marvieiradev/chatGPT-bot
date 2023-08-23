60478ce93ef18899621fc665c897172971d8d474
Marcos Vieira <marcosvieira482@gmail.com>
2023-08-23 20:55:31 -0300

    feat: update styles.css

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const chatLog = document.getElementById('chat-log');
+const userInput = document.getElementById('user-input');
+const sendButton = document.getElementById('send-button');
+const buttonIcon = document.getElementById('button-icon');
+const info = document.querySelector('.info');
